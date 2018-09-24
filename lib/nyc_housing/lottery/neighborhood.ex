ddcb11433cb9d27a32cc904f2aeede16ac969be2
Tom Schuster <tomjschuster@gmail.com>
2018-09-24 00:38:02 -0400

    remove js stuff

---
+++ lib/nyc_housing/lottery/neighborhood.ex
@@ -0,0 +1,3 @@
+defmodule NycHousing.Lottery.Neighborhood do
+  defstruct [:id, :name, :short_name, :sort_order]
+end
--- nyc_housing/lib/nyc_housing/lottery/neighborhood.ex
+++
@@ -1,5 +0,0 @@
-defmodule NycHousing.Lottery.Neighborhood do
-  alias __MODULE__
-
-  defstruct [:id, :name, :short_name, :sort_order]
-end
