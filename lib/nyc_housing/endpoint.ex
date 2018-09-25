11986b3cc1cb105e3cfa5c7f50416271ccf57b44
Tom Schuster <tomjschuster@gmail.com>
2018-09-25 00:11:52 -0400

    project json view

--- lib/nyc_housing/endpoint.ex
+++ lib/nyc_housing/endpoint.ex
@@ -2,25 +2,34 @@ defmodule NycHousing.Endpoint do
   import Plug.Conn
   alias NycHousing.Lottery.Project
 
-  def show(conn) do
-    send_resp(conn, 200, "Hello World!")
-  end
-
   def index_projects(conn) do
     projects = NycHousing.list_lottery_projects()
-    json(conn, [])
+    json(conn, Enum.map(projects, &view_project/1))
   end
 
   def show_project(conn) do
-    IO.inspect(conn)
     {id, ""} = Integer.parse(conn.params["project_id"])
-    project = NycHousing.get_lottery_project(id)
-    json(conn, %{})
+
+    case NycHousing.get_lottery_project(id) do
+      %Project{} = project -> json(conn, view_project(project))
+      nil -> send_resp(conn, 404, "Not Found")
+    end
   end
 
   defp json(conn, data) do
     conn
     |> put_resp_content_type("application/json")
     |> send_resp(conn.status || 200, Poison.encode!(data))
+  end
+
+  defp view_project(project) do
+    %{
+      id: project.id,
+      name: project.name,
+      neighborhoodId: project.neighborhood_id,
+      addresses: project.addresses,
+      startDate: project.start_date,
+      endDate: project.end_date
+    }
   end
 end
--- lib/nyc_housing/router.ex
+++ lib/nyc_housing/router.ex
@@ -2,7 +2,8 @@ defmodule NycHousing.Router do
   use Plug.Router
   plug(:match)
 
-  plug(Plug.Parsers,
+  plug(
+    Plug.Parsers,
     parsers: [:json],
     pass: ["application/json"],
     json_decoder: Poison
@@ -10,7 +11,8 @@ defmodule NycHousing.Router do
 
   plug(:dispatch)
 
+  get("/", do: NycHousing.Endpoint.index_html(conn))
   get("/projects/:project_id", do: NycHousing.Endpoint.show_project(conn))
-  # get("/projects", do: NycHousing.Endpoint.index_projects(conn))
+  get("/projects", do: NycHousing.Endpoint.index_projects(conn))
   match(_, do: send_resp(conn, 404, "Not Found"))
 end
